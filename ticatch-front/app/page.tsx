1065a2d3a2fbc52752a9f7fd63c8d7e2df58091e
Hyunsoo <imapples@naver.com>
2025-01-16 21:58:37 +0900

    test: 중단 코드 주석 처리

--- ticatch-front/app/page.tsx
+++ ticatch-front/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <button onClick={handleTrigger}>가보자</button>
-      <button onClick={handleStop}>멈추자</button>
+      {/* <button onClick={handleStop}>멈추자</button> */}
     </div>
   )
 }
