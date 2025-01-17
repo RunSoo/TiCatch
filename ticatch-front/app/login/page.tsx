e5e025cf3fee66d55f0cd7cb914c56c600ba9d9b
Hyunsoo <imapples@naver.com>
2025-01-17 17:42:14 +0900

    fix: kakao auth url 수정

--- ticatch-front/app/login/page.tsx
+++ ticatch-front/app/login/page.tsx
@@ -5,7 +5,7 @@ const LoginPage = () => {
   const KAKAO_REDIRECT_URI = process.env
     .NEXT_PUBLIC_KAKAO_REDIRECT_URI as string;
 
-  const KAKAO_AUTH_URL = `http://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URI}`;
+  const KAKAO_AUTH_URL = `http://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
   return (
     <div>
       <h1>로그인 페이지</h1>
