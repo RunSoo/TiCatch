d5bacefb065649cea6b0615c406315fcf2b5cee1
Hyunsoo <imapples@naver.com>
2025-01-20 22:55:49 +0900

    fix: api 요청 헤더에 accessToken 추가

--- ticatch-front/lib/axiosClient.ts
+++ ticatch-front/lib/axiosClient.ts
@@ -1,8 +1,21 @@
-import axios from "axios";
+import axios from 'axios';
 
 const axiosClient = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api`,
   withCredentials: true,
 });
+
+axiosClient.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem('accessToken');
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  },
+);
 
 export default axiosClient;
